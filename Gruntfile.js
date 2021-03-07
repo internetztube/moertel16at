d611cb58b26709f468f4d6fe7a0d4a35063d2072
Frederic Koeberl <frederic.koeberl@dynatrace.com>
2021-03-07 22:12:28 +0100

    Trigger build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,9 @@
 'use strict';
- 
+
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    
+
     grunt.initConfig({
         watch: {
             less: {files: "less/**/*.less", tasks: ["less"]},
